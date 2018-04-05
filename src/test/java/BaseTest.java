8ffb4772921c437a11d759b574c7e6d13b0b3ec2
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2018-04-06 01:41:03 +0300

    Update BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,21 +3,20 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-//import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.containers.BrowserWebDriverContainer;
 import com.codeborne.selenide.WebDriverRunner;
 
 public class BaseTest {
 	
-//	  @Rule
-//	  public BrowserWebDriverContainer chrome =
-//	    new BrowserWebDriverContainer()
-//  .withDesiredCapabilities(DesiredCapabilities.chrome());
+	  @Rule
+	  public BrowserWebDriverContainer chrome =
+	    new BrowserWebDriverContainer().withDesiredCapabilities(DesiredCapabilities.chrome());
 	  
-//	  @Before
-//	  public void setUp() {
-//	    RemoteWebDriver driver = chrome.getWebDriver();
-//	    WebDriverRunner.setWebDriver(driver);
-//	  }
+	  @Before
+	  public void setUp() {
+	    RemoteWebDriver driver = chrome.getWebDriver();
+	    WebDriverRunner.setWebDriver(driver);
+	  }
 
 	  @After
 	  public void tearDown() {
